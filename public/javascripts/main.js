5ea4d35523c5e3130b3921252a351426ebf3f956
Diego Ruiz <cvillian098@gmail.com>
2017-10-23 22:40:51 -0400

    adding evenOrOdd example

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -3,10 +3,20 @@ appname.controller('appCtrl', ['$scope','$http',
   function($scope,$http) {
     $scope.greeting = { text: 'Hello AngularJS Here' };
     $scope.apiCall = function(){
-		$http.get("/api/test")
-			.then(function(data) {
-		    $scope.response = data.data;
-		    console.log(data);
+		$http.get("/api/test").then(function(response) {
+		    $scope.response = response.data;
+		    console.log(response);
 		});
     };
+
+    $scope.formMessage;
+    $scope.numberInput;
+    $scope.formSubmit = function(){
+    	$http.post("/api/evenOrOdd", {
+    		numberInput: $scope.numberInput
+    	}).then(function(response) {
+    		console.log(response.data);
+    		$scope.formMessage = response.data;
+		});
+    }
 }]);
--- routes/api.js
+++ routes/api.js
@@ -6,4 +6,20 @@ router.get('/test', function(req, res, next) {
   	res.send(JSON.stringify({'test':123}));
 });
 
+router.post('/evenOrOdd', function(req, res, next) {
+	if(req && req.body && req.body.numberInput && !isNaN(req.body.numberInput)){
+		var input = parseInt(req.body.numberInput);
+		var output = input%2?"odd":"even";
+		res.setHeader('Content-Type', 'application/json');
+  		res.send(JSON.stringify({'output':output,"input":input}));
+	} 
+	else if(req && req.body && req.body.numberInput && isNaN(req.body.numberInput)) {
+		res.setHeader('Content-Type', 'application/json');
+  		res.send(JSON.stringify({'error':req.body.numberInput + " is not a number"}));
+	} else {
+		res.setHeader('Content-Type', 'application/json');
+  		res.send(JSON.stringify({'error': "'numberInput' not provided"}));
+	}
+});
+
 module.exports = router;
