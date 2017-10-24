54cf725ae7367b7c6ca46b81a460f2d03c6dd821
Diego Ruiz <cvillian098@gmail.com>
2017-10-23 22:54:41 -0400

    tweaking part1

--- helloworld/helloworld.js
+++ helloworld/helloworld.js
@@ -1,13 +1,15 @@
-const express = require('express')
-const path = require('path');
-const app = express()
+var express = require('express');
+var path = require('path');
+var app = express();
 
 // app.get('/', function (req, res) {
 //   res.send('Hello World!')
 // })
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname,  'index.html'));
+});
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-})
+  console.log('Example app listening on port 3000!');
+});
