a01623ad956c58a5c421e9cf1902afecd9f2acb8
Diego Ruiz <cvillian098@gmail.com>
2017-10-23 22:15:43 -0400

    updates and adding part1 examples

---
+++ helloworld/helloworld.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const path = require('path');
+const app = express()
+
+// app.get('/', function (req, res) {
+//   res.send('Hello World!')
+// })
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
